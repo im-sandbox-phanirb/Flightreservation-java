f070c65661f44187338eeaf99dd7d17e1c81524a
rajkumarinfomagnus <rajkumar.bommaraju@infomagnus.com>
2024-06-05 19:14:42 +0530

    commit

--- src/main/java/com/bharath/flightreservation/controllers/FlightController.java
+++ src/main/java/com/bharath/flightreservation/controllers/FlightController.java
@@ -1,7 +1,17 @@
 package com.bharath.flightreservation.controllers;
 
+import com.bharath.flightreservation.entities.Flight;
+import com.bharath.flightreservation.repos.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
 
 
 
@@ -9,10 +19,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FlightController {
 
+    @Autowired  
+    private FlightRepository flightRepository;
+
     @GetMapping("/findFlights")
     public String displayFindFlights() {
-        
-        return "findFlights"; // Return the name of the Thymeleaf template
+        return "findFlights";   
     }
 
+    @PostMapping("/findFlights")
+    public String findFlights(@RequestParam("from") String from, @RequestParam("to") String to, @RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") Date departureDate, Model model) {
+        List<Flight> flights = flightRepository.findFlights(from, to, departureDate);
+        model.addAttribute("flights", flights);
+        return "displayFlights";
+    }
 }
